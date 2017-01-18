d37fde539d8b3e27a7f1f27fdc59874170515882
Marvyn <marvyn.duvauchelle@gmail.com>
2017-01-18 10:19:03 +0100

    First commit

---
+++ src/model/User.java
@@ -0,0 +1,138 @@
+package model;
+
+import java.util.Date;
+
+public class User {
+
+	public User() {
+	}
+
+	private Long id;
+	
+	public String lastName;
+	public String firstName;
+	public String username;
+	public String gender;
+	public int age;
+	public Date birthDate;
+	public String email;
+	public String phone;
+	public String zipCode;
+	public String description;
+	public String photo;
+	
+	public User(Long id, String lastName, String firstName, String username, String gender, int age, Date birthDate,
+			String email, String phone, String zipCode, String description, String photo) {
+		this.id = id;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.username = username;
+		this.gender = gender;
+		this.age = age;
+		this.birthDate = birthDate;
+		this.email = email;
+		this.phone = phone;
+		this.zipCode = zipCode;
+		this.description = description;
+		this.photo = photo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+	
+}
